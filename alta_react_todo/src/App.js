2ab7a16131f3741e9e7bf82cbc7c6b2d7a9441c9
MohammadWawan <mohammadwawan.24@gmail.com>
2021-11-03 06:11:07 +0700

    add day12

---
+++ alta_react_todo/src/App.js
@@ -0,0 +1,13 @@
+import Home from './pages/home'
+import ToDoPages from './pages/todoPages'
+
+function App() {
+  return (
+    <div className="App">
+     <Home></Home>
+     <ToDoPages></ToDoPages>
+    </div>
+  );
+}
+
+export default App;
